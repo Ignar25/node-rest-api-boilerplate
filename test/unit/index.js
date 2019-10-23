d3bc797e1385385fffabc7820ee35d04237e70d1
Ignar25 <nacho.arias25@gmail.com>
2019-10-23 20:17:33 -0300

    Added user test

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,8 +1,36 @@
-const assert = require('chai').assert;
+const { assert } = require('chai');
+const { expect } = require('chai');
+const userController = require('../../src/application/controllers/user.controller');
 
 describe('Dummy test', () => {
   it('Should just be here as to make tests pass', (done) => {
     assert(true);
     done();
+  });
+});
+
+describe('Get Users', () => {
+  it('Should return user data', async () => {
+    const mockObj = [
+      {
+        id: 3426243159,
+        name: 'Diego Matias',
+        lastname: 'Pascual',
+        birthday: '1985-08-20',
+        is_active: true,
+        email: 'dpascual@email.com',
+        genre: 'Masculino',
+      },
+      {
+        age: 34,
+      },
+    ];
+    const mockRes = {
+      json(obj) {
+        return mockObj;
+      },
+    };
+    const data = await userController.getUsers({}, mockRes);
+    expect(data).to.be.an('Array');
   });
 });
