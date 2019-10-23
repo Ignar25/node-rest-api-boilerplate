db3b9471e8ca4dbe02dcc194c2eec6f48cfe7244
Ignar25 <nacho.arias25@gmail.com>
2019-10-23 20:32:07 -0300

    Changed config

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ const config = {
   env: process.env.NODE_ENV || 'dev',
   logging: ':method :url :status [:response-time ms] :date[web]',
   port: process.env.PORT || 3000,
+  url: 'https://2eja2nqth0.execute-api.us-east-1.amazonaws.com/api/users',
 };
 
 module.exports = config;
